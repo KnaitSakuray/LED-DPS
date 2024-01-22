76c2d7187e669f7c0d1fefbf8fb4373e59cdfb0b
Eduardo Lopez <eduardo.bonfimlopez@hotmail.com>
2024-01-22 10:52:08 -0300

    Atualização

--- LED DPS/Class/Conexao/Conexao.cs
+++ LED DPS/Class/Conexao/Conexao.cs
@@ -9,7 +9,7 @@ namespace LED_DPS.Class.Conexao
     internal class Conexao
     {
 
-        private static string _ROTA = "Data Source = 192.168.108.16\\SQLEXPRESS; Initial Catalog = DPS; Persist Security Info=True;User ID =crud_dps; Password=@UniLed!@#2023";
+        private static string _ROTA = "";
         public static string ROTA
         {
             get { return _ROTA; }
@@ -17,7 +17,7 @@ namespace LED_DPS.Class.Conexao
         }
 
 
-        private static string _PRINT_SERVER = @"\\EXT-AV-01\";
+        private static string _PRINT_SERVER = "";
         public static string PRINT_SERVER
         {
             get { return _PRINT_SERVER; }
